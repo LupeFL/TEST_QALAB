cc9b50f36cedbd9440c501622adf168b48684d6a
LupeFL <116371789+LupeFL@users.noreply.github.com>
2022-10-26 19:39:51 -0500

    Cambio web
    Se declaró la variable j.

--- Proyecto_dev/src/Main.java
+++ Proyecto_dev/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola desarrollador");
+        
+        //Cambio realizado desde la web
+        
+        double j=0;
     }
 }
