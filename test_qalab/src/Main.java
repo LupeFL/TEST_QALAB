0d4581d3acf0907b953972dfe749ffde6d7d5e7a
LupeFL <ysabelfl@outlook.es>
2022-10-26 19:27:32 -0500

    Archivo creado localmente

---
+++ test_qalab/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Entrada desde IntelliJ!");
+
+        //es una entrada desde el directorio local
+    }
+}
